0d21c906d96ffcf23b1889fe0ae84d6295f9f2ae
weijunhao <weijunhao@etonetech.com>
2025-09-01 19:32:24 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/views": path.resolve(__dirname, "./src/views"),
+      "@/modules": path.resolve(__dirname, "./src/modules"),
+      "@/routers": path.resolve(__dirname, "./src/routers"),
+      "@/stories": path.resolve(__dirname, "./src/stories"),
+      "@/assets": path.resolve(__dirname, "./src/assets")
+      "@/common": path.resolve(__dirname, "./src/common")
+    },
+  }
+})
